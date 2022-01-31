4de50de2081062c948a0c4d2f559d8d4089080e0
Drew Maring <drewmaring@google.com>
2022-01-31 16:07:53 -0700

    Add app.py and init.sh

---
+++ app.py
@@ -0,0 +1,25 @@
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_default():
+    url = 'http://metadata.google.internal/computeMetadata/v1'
+    headers = {'Metadata-Flavor': 'Google'}
+    default_info = ['/instance/hostname', '/instance/machine-type', '/instance/zone']
+    return_text = ''
+    for subpath in default_info:
+        new_url = url + subpath
+        r = requests.get(new_url, headers=headers)
+        return_text += (r.text + "\n")
+    return(return_text)
+
+
+@app.route("/<path:subpath>")
+def get_info(subpath):
+    url = 'http://metadata.google.internal/computeMetadata/v1/'
+    headers = {'Metadata-Flavor': 'Google'}
+    url += subpath
+    r = requests.get(url, headers=headers)
+    return(r.text)
---
+++ init.sh
@@ -0,0 +1,1 @@
+# VM requirements script
