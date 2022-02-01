cafaec3d83444f5ddbc780a4b5cf32d44eda1933
Drew Maring <drewmaring@google.com>
2022-01-31 17:42:40 -0700

    Update init.sh with python requirements install

--- init.sh
+++ init.sh
@@ -1,1 +1,13 @@
-# VM requirements script
+# VM preparation script
+
+# Update apt-get
+sudo apt-get update
+
+# Install Python pip3
+sudo apt-get -y install python-pip3
+
+# Install Python requirements
+pip3 install -r requirements.txt
+
+
+
